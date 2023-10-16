5e5e9eddad89036bb2b924020e37f2295e6388f5
ljperalta <leoperalta.lp24@gmail.com>
2023-10-16 17:18:08 -0300

    Fin archivo de conexion

--- conexion.php
+++ conexion.php
@@ -3,4 +3,7 @@ $dbHost = "localhost";
 $dbUser = "root";
 $dbPass = "";
 $dbName = "labo4";
+
+$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+if(!$conn) die("No hay conexion: ".mysqli_connect_error());
 ?>
