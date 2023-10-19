36f159ab3726f2b8fe1240b680bf81c6f9d53990
ljperalta <leoperalta.lp24@gmail.com>
2023-10-18 23:55:58 -0300

    prueba de printeo de tabla reserva

--- funciones.php
+++ funciones.php
@@ -7,6 +7,14 @@ function setReserva(){
     if($res) return "OK";
     else return "ERROR";
 }
+function getReservas(){
+    include_once("conexion.php");
+    $sql = "SELECT * FROM reserva ORDER BY fecha_inicio ASC LIMIT 3";
+    $res = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_row($res);
+
+    if(mysqli_num_rows($res) > 0) return $row;
+}
 $respuesta = setReserva();
 echo $respuesta."\n";
 ?>
