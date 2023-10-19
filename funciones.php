c50f82f68a285f98766b747f0b9a9cc21f905aac
ljperalta <leoperalta.lp24@gmail.com>
2023-10-18 22:03:01 -0300

    prueba de insert a tabla reserva

--- funciones.php
+++ funciones.php
@@ -1,10 +1,12 @@
 <?php
 function setReserva(){
     include_once("conexion.php");
-    $sql = "INSERT INTO reserva (cuil, fecha_inicio, fecha_fin, habitacion, servicio, vendedor) VALUES 
-            (20402346176, '2023-10-02', '2023-10-12', 2, 3, 1)";
+    $sql = "INSERT INTO reserva (cuil, fecha_inicio, fecha_fin, habitacion, adicional, vendedor) VALUES 
+            (20402346176, '2023-10-02', '2023-10-12', 99, 99, 2)";
     $res = mysqli_query($conn, $sql);
     if($res) return "OK";
     else return "ERROR";
 }
+$respuesta = setReserva();
+echo $respuesta."\n";
 ?>
