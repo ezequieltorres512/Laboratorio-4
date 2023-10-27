09daf300bb6455860a22a015100b8a3e4cf24c8a
ljperalta <leoperalta.lp24@gmail.com>
2023-10-27 19:31:53 -0300

    Envio los datos para update

--- controlador.php
+++ controlador.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
 if($_POST['llegada']){
-    print_r($_POST['origen'] == 8);
-    if (true) {
+    //print_r();
+    if ($_POST['origen'] == 8) {
         $sql = "UPDATE reserva SET fecha_inicio = '".$_POST['llegada']."', fecha_fin = '".$_POST['salida']."', tipoHabitacion = ".$_POST['tipoH']." WHERE id_reserva = 6";
         $res = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_array($conn, $res);
-        echo $_SESSION['user']."-----\n$sql\n";
-        //header("Location: confirmacion.html");
+        //echo $_SESSION['user']."-----\n$sql\n";
+        if($res == true) header("Location: confirmacion.html");
     }else{
         header("Location: menu2.html");
     }
