b55f02bbe417fe62ee2912337cb61773857ff2ad
ljperalta <leoperalta.lp24@gmail.com>
2023-10-29 20:22:23 -0300

    Datos de reserva para el login

--- login.php
+++ login.php
@@ -7,13 +7,16 @@ $pass = $_POST["txtpassword"];
 $query = mysqli_query($conn,"SELECT * FROM usuarios WHERE email = '$email' and clave = '".$pass."'");
 $nr = mysqli_num_rows($query);
 $row = mysqli_fetch_array($query);
-
+$usuario = $row['id_usuario'];
 if($nr == 1)
 {
 	header("Location: menu2.html");
 	session_start();
 	$_SESSION['email']  = $email;
-	$_SESSION['usuario'] = $row['id_usuario'];
+	$_SESSION['usuario'] = $usuario;
+	$queryR = "SELECT * FROM reserva WHERE id_usuario = $usuario";
+	$queryReserva = mysqli_query($conn, $queryR);
+	$rowReserva = mysqli_fetch_array($queryReserva);
 }
 else if ($nr == 0) 
 {
