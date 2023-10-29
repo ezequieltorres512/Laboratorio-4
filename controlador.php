ea9e267549046c6ecb84a7a1ef9b816c2903be1c
ljperalta <leoperalta.lp24@gmail.com>
2023-10-29 11:34:06 -0300

    Datos de login

--- controlador.php
+++ controlador.php
@@ -4,7 +4,7 @@ include_once("conexion.php");
 if($_POST['llegada']){
     //print_r();
     if ($_POST['origen'] == 8) {
-        $sql = "UPDATE reserva SET fecha_inicio = '".$_POST['llegada']."', fecha_fin = '".$_POST['salida']."', tipoHabitacion = ".$_POST['tipoH']." WHERE id_reserva = 6";
+        $sql = "UPDATE reserva SET fecha_inicio = '".$_POST['llegada']."', fecha_fin = '".$_POST['salida']."' WHERE id_reserva = 6";
         $res = mysqli_query($conn, $sql);
         //echo $_SESSION['user']."-----\n$sql\n";
         if($res == true) header("Location: confirmacion.html");
--- login.php
+++ login.php
@@ -1,17 +1,19 @@
 <?php
 include_once("conexion.php");
 
-$nombre = $_POST["txtusuario"];
+$email = $_POST["txtusuario"];
 $pass = $_POST["txtpassword"];
 
-$query = mysqli_query($conn,"SELECT * FROM usuarios WHERE id_usuario = $nombre and clave = '".$pass."'");
+$query = mysqli_query($conn,"SELECT * FROM usuarios WHERE email = '$email' and clave = '".$pass."'");
 $nr = mysqli_num_rows($query);
+$row = mysqli_fetch_array($query);
 
 if($nr == 1)
 {
 	header("Location: menu2.html");
 	session_start();
-	$_SESSION['user']  = $pass;
+	$_SESSION['email']  = $email;
+	$_SESSION['usuario'] = $row['id_usuario'];
 }
 else if ($nr == 0) 
 {
