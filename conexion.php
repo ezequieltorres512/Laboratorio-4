36f1cf56689740b6409472dcc21a95e4ae2da454
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-10-29 23:23:47 -0300

    vuelvo conexion atras

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$dbHost = "boqabnrvhlsejwww4hty-mysql.services.clever-cloud.com";
-$dbUser = "uya9bsrnyguckms9";
-$dbPass = "uPPq9dkQyxvMqLdwmi7Y";
-$dbName = "boqabnrvhlsejwww4hty";
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPass = "";
+$dbName = "labo4";
 
 $conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
 if(!$conn) die("No hay conexion: ".mysqli_connect_error());
