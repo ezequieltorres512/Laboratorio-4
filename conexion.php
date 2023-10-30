339cd6e8470b5ce111027e9a34bfa233cfe73a90
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-10-29 23:22:04 -0300

    pruebo bd en linea

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$dbHost = "localhost";
-$dbUser = "root";
-$dbPass = "";
-$dbName = "labo4";
+$dbHost = "boqabnrvhlsejwww4hty-mysql.services.clever-cloud.com";
+$dbUser = "uya9bsrnyguckms9";
+$dbPass = "uPPq9dkQyxvMqLdwmi7Y";
+$dbName = "boqabnrvhlsejwww4hty";
 
 $conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
 if(!$conn) die("No hay conexion: ".mysqli_connect_error());
