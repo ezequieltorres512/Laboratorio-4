8b58ad76703f5bf2714c00e7b9dc21b7f5a9b1b6
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-10-29 22:09:38 -0300

    borro variables que no se utilizan

--- publicidad.php
+++ publicidad.php
@@ -1,9 +1,6 @@
 <?php
 include_once("conexion.php");
 
-$email = $_POST["txtusuario"];
-$pass = $_POST["txtpassword"];
-
 $query = mysqli_query($conn,"SELECT * FROM canalesdifusion");
 $nr = mysqli_num_rows($query);
 echo "<select name='canal_difusion' id='canal_difusion'>";
