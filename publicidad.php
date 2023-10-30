7f2442195d008e0093c10beebee4dc50e23f94cc
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-10-29 21:52:06 -0300

    agrego altas de reserva

---
+++ publicidad.php
@@ -0,0 +1,21 @@
+<?php
+include_once("conexion.php");
+
+$email = $_POST["txtusuario"];
+$pass = $_POST["txtpassword"];
+
+$query = mysqli_query($conn,"SELECT * FROM canalesdifusion");
+$nr = mysqli_num_rows($query);
+echo "<select name='canal_difusion' id='canal_difusion'>";
+while($row = mysqli_fetch_assoc($query)){
+      echo "<option value=".$row['id'].">".$row['descripcion']."</option>";
+     // print_r($row);
+}
+echo "</select>";
+if($nr == 0) 
+{
+	//header("Location: login.html");
+	//echo "No ingreso"; 
+	echo "<script> alert('Error');window.location= 'index.html' </script>";
+}
+?>
