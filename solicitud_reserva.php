974a0d470c5f26814604d887225714b0e931c729
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-10-29 22:58:19 -0300

    agrego php habitacion y cargo select de bdd

--- solicitud_reserva.php
+++ solicitud_reserva.php
@@ -39,11 +39,9 @@
                   <p>Fecha de Salida<br>
                   <input type="date" name="salida" id="salida">
                   <p>Seleccione el tipo de habitacion<br>
-                  <select name="tipoH" id="tipoH">
-                  <option value=""></option>
-                  <option value="1">King</option>
-                  <option value="2">Classic</option>
-                  </select>
+                  <?php
+                  require("habitacion.php");
+                  ?>
                   <p>Seleccione como nos conocio<br>
                   <?php
                   require("publicidad.php");
