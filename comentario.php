22862fde21274fd89c134b4e3791d2544ccd0bd3
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-11-01 18:20:25 -0300

    subo cambios para que guarde form

---
+++ comentario.php
@@ -0,0 +1,19 @@
+<?php      
+session_start();
+/*termina function */
+      include_once("conexion.php");
+      $nombre = $_POST["nombre"];
+      $correo = $_POST["correo"];
+      $comentario = $_POST["comentario"];
+
+      $query = mysqli_query($conn,"INSERT INTO comentarios (persona,correo,comentario) VALUES ('$persona','$correo','$comentario')");
+
+                                               
+      if($query){
+            require("redireccion_mensaje.php");
+      }else{
+            echo mysqli_error($conn);
+      }
+
+
+?>
--- inicio.php
+++ inicio.php
@@ -94,6 +94,7 @@
     </div>
     <div class="contact-form">
         <form action="enviar_correo.php">
+            <input type="hidden" name="motivo" value="Envio de comentario">
             <input type="text" name='nombre 'placeholder="Tu nombre">
             <input type="email" name="correo" id="" placeholder="Tu email">
             <textarea name="comentario" id="comentario" cols="30" rows="10" placeholder="Dejame tu mensaje que me conindextacto a la brevedad"></textarea>
