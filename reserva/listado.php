c62c37a2256289b9234a89fc214901615c229356
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-11-02 23:22:52 -0300

    subo cambios

---
+++ reserva/listado.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel la 7ma</title>
+    <link rel="stylesheet" href="../estilos/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">    
+    
+</head>
+
+<body>
+ 
+<header>
+    <a href="#" class="logo">La 7ma <span>Hotel</span></a>
+
+    <div class="bx bx-menu" id="menu-icon"></div>
+
+    <ul class="navbar">
+
+        <li><a href="../inicio.php">Inicio</a></li>
+
+        <li><a href="#about">Reservas</a></li>
+        <li><a href="#skills">Instalaciones</a></li>
+        <li><a href="#services">Servicios</a></li>
+        <li><a href="#contact">Contacto</a></li>
+        <div class="bx bx-moon" id="darkmode"></div>
+    </ul>
+</header>
+<section class="about" id="about">
+
+    <div id="mayor" style='margin-top:2%'>		            
+        <iframe src="tabla.php" width="80%" height="100%"></iframe>
+	</div>
+</section>
+
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="../js/script.js"></script>
+
+
+</html>
