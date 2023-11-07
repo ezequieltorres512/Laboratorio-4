c479929b032cbf2e09becc910070216e82a2ea17
ljperalta <leoperalta.lp24@gmail.com>
2023-11-06 21:03:50 -0300

    Subo agregado de campos para alta de reserva

--- reserva/alta_reserva.php
+++ reserva/alta_reserva.php
@@ -5,7 +5,10 @@ include_once("../conexion.php");
 //print_r($_POST);
 //print_r($_SESSION);
 //echo "</pre>";
-
+$apellido = $_POST["ape"];
+$nombre = $_POST["nom"];
+$correo = $_POST["correo"];
+$tel = $_POST["tel"];
 $llegada = $_POST["llegada"];
 $salida = $_POST["salida"];
 $tipo_habitacion = $_POST["tipoH"];
@@ -13,9 +16,9 @@ $canal = $_POST["canal_difusion"];
 $precio = substr($_POST["precio"],1,strlen($_POST['precio']));
 $id_usuario = $_SESSION['usuario'];
 $adicional = 0;
-$vendedor=0;
-$query = mysqli_query($conn,"INSERT INTO reserva (precio,id_usuario,fecha_inicio, fecha_fin, tipoHabitacion, adicional,vendedor, conocidosPor) VALUES 
-                                                ($precio,$id_usuario,'$llegada','$salida',$tipo_habitacion,$adicional,$vendedor,$canal)");
+$vendedor=0;      
+$query = mysqli_query($conn,"INSERT INTO reserva (precio,id_usuario,apellido, nombre, telefono, correo,fecha_inicio, fecha_fin, tipoHabitacion, adicional,vendedor, conocidosPor) VALUES 
+                                                ($precio,$id_usuario,'$apellido','$nombre',$tel,'$correo','$llegada','$salida',$tipo_habitacion,$adicional,$vendedor,$canal)");
 if($query){
     //  header("Location: inicio.php");
 
