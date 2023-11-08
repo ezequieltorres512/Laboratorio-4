456a2050c8c56db956338169d828fb8007d434d6
ljperalta <leoperalta.lp24@gmail.com>
2023-11-07 22:56:23 -0300

    Subo altas de habitacion y sus tipos

--- reserva/select_reservas.php
+++ reserva/select_reservas.php
@@ -6,7 +6,7 @@ $query = mysqli_query($conn,"SELECT * FROM reserva $where order by fecha_inicio 
 $nr = mysqli_num_rows($query);
 $precios=array();
 
-echo "<select name='id_seleccionado' id='id_seleccionado'>";
+echo "<select name='id_seleccionado' id='id_seleccionado' required>";
 
 echo "<option value=''></option>";
 while($row = mysqli_fetch_assoc($query)){
