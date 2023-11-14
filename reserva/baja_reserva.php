e6213c66ca0b13689977b1a1ec53a6528e3caec4
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-11-13 22:28:27 -0300

    subo cambios a rama

--- reserva/baja_reserva.php
+++ reserva/baja_reserva.php
@@ -4,7 +4,7 @@ include_once("../conexion.php");
 $id = $_POST["reservas"];
 $query = mysqli_query($conn," UPDATE reserva SET baja ='1' WHERE reserva.id_reserva= $id");
 if($query){
-      require("../mensajes/redireccion_mensaje.php");
+      require("../reserva/listado.php");
 }else{
       echo mysqli_error($conn);
 }
