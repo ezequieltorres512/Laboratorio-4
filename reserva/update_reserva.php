29beacad7631a499ac1f449576d6218a2fbee495
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-11-14 23:06:25 -0300

    Modificaciones para lograr update

--- reserva/update_reserva.php
+++ reserva/update_reserva.php
@@ -1,20 +1,19 @@
 <?php
 session_start();
 include_once("../conexion.php");
-    echo "<br>";
-echo "<br>";
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+//    echo "<br>";
+//echo "<br>";
+//echo "<pre>";
+//print_r($_POST);
+//echo "</pre>";
     preg_match('/\d+/', $_POST['precio'], $matches);
     $precio = $matches[0];
     $sql = "UPDATE reserva SET fecha_inicio = '".$_POST['fInicioI']."', fecha_fin = '".$_POST['fFinI']."', tipoHabitacion = ".$_POST['tipoH'].", precio = $precio WHERE id_reserva = ".$_POST['id_seleccionado'];
-    echo "$sql<br>";
-    //$res = mysqli_query($conn, $sql);
-    echo "-----\n$sql\n";
-    echo "<pre>";
-        print_r($_SESSION);
-        echo "</pre>";
+ //   //$res = mysqli_query($conn, $sql);
+   // echo "-----\n$sql\n";
+   // echo "<pre>";
+    //    print_r($_SESSION);
+     //   echo "</pre>";
     $query = mysqli_query($conn,$sql);                                           
     if($query){
             require("../mensajes/redireccion_mensaje.php");
