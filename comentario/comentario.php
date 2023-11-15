f2a66ad19abdecaf5e9562710fee8717a94d17f5
Ezequiel Torres <74984809+ezequieltorres512@users.noreply.github.com>
2023-11-15 09:31:40 -0300

    Update comentario.php
    Envio de mail

--- comentario/comentario.php
+++ comentario/comentario.php
@@ -5,9 +5,22 @@
       $correo = $_POST["correo"];
       $comentario = $_POST["comentario"];
 
+      /*envio de mail*/
+      $destino="admin@admin";
+      $asunto="Contacto desde el sitio";
+      $mensaje="Nombre: ".$persona." Email: ".$correo." Mensaje: ".$comentario;
+      $header="From: ".$persona."<".$correo.">";
+      $enviado = mail($destino,$asunto,$mensaje,$header);
+      
+      if($enviado == true){
+      	echo "Su mensaje ha sido enviado.";
+      }else{
+      	echo "Hubo un error en el envio del mail.";
+      }
+
       $query = mysqli_query($conn,"INSERT INTO comentarios (persona,correo,comentario) VALUES ('$persona','$correo','$comentario')");
 
-
+      
                                                
       if($query){
             require("../mensajes/redireccion_mensaje.php");
