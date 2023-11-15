a2ec689b46d267ef239be0b6bd7c1444574bd8db
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-11-14 22:24:17 -0300

    agrego calcular precio

--- habitacion/select_habitacion.php
+++ habitacion/select_habitacion.php
@@ -5,23 +5,18 @@ $query = mysqli_query($conn,"SELECT * FROM tipohabitacion");
 $nr = mysqli_num_rows($query);
 $precios=array();
 
-echo "<select name='tipoH' id='tipoH' required>";
+//echo "<select name='tipoH' id='tipoH' required>";
 
-echo "<option value=''></option>";
+//echo "<option value=''></option>";
 while($row = mysqli_fetch_assoc($query)){
-    echo "<option value=".$row['id'].">".$row['titulo']."</option>";
+  //  echo "<option value=".$row['id'].">".$row['titulo']."</option>";
     // print_r($row);
     $precios[$row['id']]=$row['precio'];
 }
-echo "</select>";
+//echo "</select>";
 foreach($precios as $id_precio => $cantidad){
     echo "<input type='hidden' name='$id_precio' value='$cantidad'>";
 }
 //echo "<pre>";print_r($precios);echo"</pre>";
-if($nr == 0) 
-{
-	//header("Location: login.html");
-	//echo "No ingreso"; 
-	echo "<script> alert('Error Habitacion');window.location= '../index.php' </script>";
-}
+
 ?>
