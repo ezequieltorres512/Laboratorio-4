0855111228d13d09b5198164142cdbc664f561bc
ljperalta <leoperalta.lp24@gmail.com>
2023-11-15 19:55:18 -0300

    subo update

--- reserva/update_reserva.php
+++ reserva/update_reserva.php
@@ -1,19 +1,19 @@
 <?php
 session_start();
 include_once("../conexion.php");
-    echo "<br>";
-echo "<br>";
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+//     echo "<br>";
+// echo "<br>";
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
     preg_match('/\d+/', $_POST['precio'], $matches);
     $precio = $matches[0];
     $sql = "UPDATE reserva SET fecha_inicio = '".$_POST['fInicioI']."', fecha_fin = '".$_POST['fFinI']."', tipoHabitacion = ".$_POST['tipoH'].", precio = $precio WHERE id_reserva = ".$_POST['id_seleccionado'];
     $query = mysqli_query($conn,$sql);
-    echo "-----\n$sql\n";
-    echo "<pre>";
-        print_r($_SESSION);
-        echo "</pre>";
+//     echo "-----\n$sql\n";
+//     echo "<pre>";
+//         print_r($_SESSION);
+//         echo "</pre>";
         if(isset($_POST['habitacion_asignada'])){
                 $sql1 = 'update reserva set habitacion = '.$_POST['habitacion_asignada'];
                 $query1 = mysqli_query($conn,$sql1);
