06d90aae7291f562e4c2e004988c3014d6b43532
ljperalta <leoperalta.lp24@gmail.com>
2023-11-17 13:58:46 -0300

    correcciones en listado-baja

--- reserva/baja_reserva.php
+++ reserva/baja_reserva.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+//session_start();
 include_once("../conexion.php");
 $id = $_POST["reservas"];
 $query = mysqli_query($conn," UPDATE reserva SET baja ='1' WHERE reserva.id_reserva= $id");
