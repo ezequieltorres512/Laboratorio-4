6510d8ba0a465a6d67beb6fced0901c9610167f6
ljperalta <leoperalta.lp24@gmail.com>
2023-11-18 12:25:41 -0300

    correcciones en solicitud_modif

--- publicidad/publicidad.php
+++ publicidad/publicidad.php
@@ -4,8 +4,9 @@ include_once("../conexion.php");
 $query = mysqli_query($conn,"SELECT * FROM canalesdifusion");
 $nr = mysqli_num_rows($query);
 echo "<select name='canal_difusion' id='canal_difusion'>";
-while($row = mysqli_fetch_assoc($query)){
-      echo "<option value=".$row['id'].">".$row['descripcion']."</option>";
+echo "<option></option>";
+while($row = mysqli_fetch_assoc($query)){  
+	echo "<option value=".$row['id'].">".$row['descripcion']."</option>";
      // print_r($row);
 }
 echo "</select>";
--- reserva/update_reserva.php
+++ reserva/update_reserva.php
@@ -1,31 +1,28 @@
 <?php
 session_start();
 include_once("../conexion.php");
-//     echo "<br>";
-// echo "<br>";
- //echo "<pre>";
- //print_r($_POST);
- //echo "</pre>";
+//  echo "<pre>";
+//  print_r($_POST);
+//  echo "</pre>";
 
     preg_match('/\d+/', $_POST['precio'], $matches);
     $precio = $matches[0];
     $sql = "UPDATE reserva SET fecha_inicio = '".$_POST['fInicioI']."', fecha_fin = '".$_POST['fFinI']."', tipoHabitacion = ".$_POST['tipoH'].", precio = $precio WHERE id_reserva = ".$_POST['id_seleccionado'];
-    echo "->$sql";//exit();
+    $sql = "UPDATE reserva SET";
+    $sql .= (isset($_POST['fInicioI']) && $_POST['fInicioI'] != '')? " fecha_inicio = '".$_POST['fInicioI']."', ": "";
+    $sql .= (isset($_POST['fFinI']) && $_POST['fFinI'] != '')? " fecha_fin = '".$_POST['fFinI']."', ": "";
+    $sql .= (isset($_POST['tipoH']) && $_POST['tipoH'] != '')? " tipoHabitacion = ".$_POST['tipoH'].", precio = $precio, ": "";
+    $sql .= (isset($_POST['habitacion_asignada']) && $_POST['habitacion_asignada'] != '')? " habitacion = '".$_POST['habitacion_asignada']."', ": "";
+    $sql .= (isset($_POST['canal_difusion']) && $_POST['canal_difusion'] != '')? " conocidosPor = ".$_POST['canal_difusion']." ": "";
+
+    $sql .= "WHERE id_reserva = ".$_POST['id_seleccionado'];
+    $sql = str_replace(", WHERE", " WHERE", $sql);
+    //echo "->$sql";//exit();
     $query = mysqli_query($conn,$sql);
-//     echo "-----\n$sql\n";
-//     echo "<pre>";
-//         print_r($_SESSION);
-//         echo "</pre>";
-        if(isset($_POST['habitacion_asignada'])){
-                $sql1 = 'update reserva set habitacion = '.$_POST['habitacion_asignada'].' where id_reserva = '.$_POST['id_seleccionado'];
-                $query1 = mysqli_query($conn,$sql1);
-        }else{
-                $query1 = true;
-        }
                                                
-    if($query && $query1){
-            require("../mensajes/redireccion_mensaje.php");
-    }else{
-            echo mysqli_error($conn);
-    }
+     if($query){
+             require("../mensajes/redireccion_mensaje.php");
+     }else{
+             echo mysqli_error($conn);
+     }
 ?>
