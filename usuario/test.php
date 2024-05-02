e341e7d3e0ae8525550c8df78530617a425bd780
ljperalta <leoperalta.lp24@gmail.com>
2024-05-02 13:40:59 -0300

    test

--- usuario/test.php
+++ usuario/test.php
@@ -1,7 +1,11 @@
 <?php
 //phpinfo();
-$conn = mysqli_connect("localhost","root","","labo4");
-if(!$conn) die("No hay conexion: ".mysqli_connect_error());
-else echo "Conexion correcta!!<br>";
+// $conn = mysqli_connect("localhost","root","","labo4");
+// if(!$conn) die("No hay conexion: ".mysqli_connect_error());
+// else echo "Conexion correcta!!<br>";
+$mysqli = new mysqli("localhost","root","","labo4");
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
+}
 echo "Inicio de test1".PHP_EOL;
 ?>
