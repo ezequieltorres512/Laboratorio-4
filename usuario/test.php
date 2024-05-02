d94a7c58f6a4a453526dda2aa10bca08b5548de9
ljperalta <leoperalta.lp24@gmail.com>
2024-05-02 13:42:39 -0300

    test

--- usuario/test.php
+++ usuario/test.php
@@ -6,6 +6,8 @@
 $mysqli = new mysqli("localhost","root","","labo4");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
+}else{
+    echo "......<br>";    
 }
 echo "Inicio de test1".PHP_EOL;
 ?>
