18b48c48bf5c059a87f70684c03bd11d1a924999
ljperalta <leoperalta.lp24@gmail.com>
2024-05-02 12:34:24 -0300

    test

--- usuario/test.php
+++ usuario/test.php
@@ -2,6 +2,6 @@
 //phpinfo();
 $conn = mysqli_connect("localhost","root","","labo4");
 if(!$conn) die("No hay conexion: ".mysqli_connect_error());
-else echo "Conexion correcta!!".PHP_EOL;
+else echo "Conexion correcta!!<br>";
 echo "Inicio de test1".PHP_EOL;
 ?>
